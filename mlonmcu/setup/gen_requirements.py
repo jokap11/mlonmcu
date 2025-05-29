4aad67aa705fef7526ff77b7434b55b5325221fe
jokap11 <johannes.kappes@tuwien.ac.at>
2025-05-29 09:15:30 +0200

    [req_gen] set numpy to version support np.float64

--- mlonmcu/setup/gen_requirements.py
+++ mlonmcu/setup/gen_requirements.py
@@ -228,7 +228,7 @@ CONSTRAINTS = [
     ("kconfiglib", "==13.7.1"),
     ("matplotlib", None),
     ("networkx", None),
-    ("numpy", None),
+    ("numpy", "==1.26.4"),
     ("onnx", None),
     ("openpyxl", None),
     ("ortools", None),
